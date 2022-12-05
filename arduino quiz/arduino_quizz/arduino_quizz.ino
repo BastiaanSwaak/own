13917adc3b761ed83259cd4233a59526497ccec8
BastiaanSwaak <74536493+BastiaanSwaak@users.noreply.github.com>
2022-12-05 16:14:13 +0100

    Add files via upload

--- arduino quiz/arduino_quizz/arduino_quizz.ino
+++ arduino quiz/arduino_quizz/arduino_quizz.ino
@@ -1,69 +1,79 @@
-#include "Quiz.cpp"
+/* #include "Quiz.cpp"
 
-#include <Wire.h> 
+#include <Wire.h>
 
 #include "rgb_lcd.h"
+*/
+const int buttonPin1 = 9;
+const int buttonPin2 = 10;
+const int buttonPin3 = 11;
+const int buttonPin4 = 12;
+const int ledPin = 6;
 
-const int buttonPin1 =3;
-const int buttonPin2 =4;
-const int buttonPin3 =5;
-const int buttonPin4 =6;
-const int ledPin = 7;
 int state = 0;
 int vragenArray[10] = {};
 int aantalVragen = 1;
+int newButtonPressed = false;
+int i = 0;
 
 
 void setup() {
-  lcd.begin(16, 2);
-  
+  //  lcd.begin(16, 2);
+
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
   pinMode(buttonPin4, INPUT);
-  
+
   pinMode(ledPin, OUTPUT);
 
 
- }
+}
 
 void loop() {
 
-vragenArray[0] = Vraag vraag1("2 + 2", "4");
+  // vragenArray[0] = Vraag vraag1("2 + 2", "4");
 
+  bool button1 = digitalRead(buttonPin1);
+  bool button2 = digitalRead(buttonPin2);
+  bool button3 = digitalRead(buttonPin3);
+  bool button4 = digitalRead(buttonPin4);
 
-
-if (state == 0){
-  
-    while(i < aantalVragen){
-      int newButtonPressed = false;
-      if (knop ingedrukt){
-        newButtonPressed = true;
-        }
-       if(newButtonPressed && input == vragenArray[i].antwoord){
-            vragenArray[i].goed = true;
-         }
-          if(newButtonPressed && vragenArray[i].goed == false){
-            i = 0; 
-            newButtonPressed = false;
-            input reset
-            }
-          if(newButtonPressed && vragenArray[i].goed == true){
-            i++;
-            newButtonPressed = false;
-            input resetar
-            }
-      
-      }
-
-
-
-
-  
-  
-  
+  if (button1 || button2 || button3 || button4) {
+    newButtonPressed = true;
+  } else {
+    newButtonPressed = false;
   }
-  
+
+  digitalWrite(ledPin, newButtonPressed);
+  /*
+    if (state == 0){
+
+      while(i < aantalVragen){
+         if(newButtonPressed && input == vragenArray[i].antwoord){
+              vragenArray[i].goed = true;
+           }
+            if(newButtonPressed && vragenArray[i].goed == false){
+              i = 0;
+              newButtonPressed = false;
+              input reset
+              }
+            if(newButtonPressed && vragenArray[i].goed == true){
+              i++;
+              newButtonPressed = false;
+              input reset
+              }
+
+        }
+
+
+
+
+
+
+
+    }
+  */
 
 
 }
